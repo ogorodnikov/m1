87e30a5c53e0d96f9f6160c982ea2f60ace77315
ogoro <mi.ogoro@gmail.com>
2021-06-29 23:16:20 +0000

    add get user_pool and user_pool_client ids

--- app/core-service/core/routes.py
+++ app/core-service/core/routes.py
@@ -26,27 +26,51 @@ def load_user(user_id):
 
 
 USER_POOL = 'm1-user-pool'
-
+USER_POOL_CLIENT = 'm1-user-pool-client'
+USER_POOL_CLIENT_SECRET = ''
 
 client = boto3.client('cognito-idp')
 
-def get_user_pool_id(USER_POOL):
+
+def get_user_pool_id(user_pool):
 
     user_pool_response = client.list_user_pools(MaxResults=60)
     
     user_pools = user_pool_response['UserPools']
     
     user_pool_id = next(attribute['Id'] for attribute in user_pools
-                        if attribute['Name'] == USER_POOL)
+                        if attribute['Name'] == user_pool)
                         
     return user_pool_id
     
 
+def get_user_pool_clients(user_pool_id):
+
+    user_pool_clients_response = client.list_user_pool_clients(UserPoolId=user_pool_id,
+                                                               MaxResults=60)
+                                                        
+    # print("UPC", user_pool_clients_response)
+    
+    user_pool_clients = user_pool_clients_response['UserPoolClients']
+    
+    # print(user_pool_clients)
+    
+    user_pool_client_id = next(attribute['ClientId'] for attribute in user_pool_clients
+                               if attribute['ClientName'] == USER_POOL_CLIENT)
+                        
+    return user_pool_client_id
+    
+user_pool_id = get_user_pool_id(USER_POOL)
+user_pool_client_id = get_user_pool_clients(user_pool_id)
+
+# print('UPCI', user_pool_client_id)
+
+
 app.config['AWS_DEFAULT_REGION'] = 'us-east-1'
 app.config['AWS_COGNITO_DOMAIN'] = 'auth.ogoro.me'
-app.config['AWS_COGNITO_USER_POOL_ID'] = get_user_pool_id(USER_POOL)
-app.config['AWS_COGNITO_USER_POOL_CLIENT_ID'] = '19a0cvebjtej10gesr5r94qfqv'
-app.config['AWS_COGNITO_USER_POOL_CLIENT_SECRET'] = ''
+app.config['AWS_COGNITO_USER_POOL_ID'] = user_pool_id
+app.config['AWS_COGNITO_USER_POOL_CLIENT_ID'] = user_pool_client_id
+app.config['AWS_COGNITO_USER_POOL_CLIENT_SECRET'] = USER_POOL_CLIENT_SECRET
 app.config['AWS_COGNITO_REDIRECT_URL'] = 'https://9bca7b3479d64496983d362806a38873.vfs.cloud9.us-east-1.amazonaws.com/logged-in'
 
 
