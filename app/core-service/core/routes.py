22024c17e3dce5e35f22a77351bfa62bf851dfcc
ogoro <mi.ogoro@gmail.com>
2021-07-07 09:49:25 +0000

    facebook idp - token received

--- app/core-service/core/routes.py
+++ app/core-service/core/routes.py
@@ -20,7 +20,26 @@ def login():
     
     if request.method == 'GET':
         
-        return render_template("login.html", referrer=request.referrer)
+        code = request.args.get('code')
+        
+        if code:
+            
+            flash(f"Code: {code}", category='info')
+            
+            token_endpoint = 'https://graph.facebook.com/oauth/access_token'
+            
+            parameters = {'client_id': '355403372591689',
+                          'client_secret': '14a6e02a55e3c801993f58882e1b4ea1',
+                          'grant_type': 'authorization_code',
+                          'redirect_uri': 'https://9bca7b3479d64496983d362806a38873.vfs.cloud9.us-east-1.amazonaws.com/login',
+                          'code': code}
+            
+            token_response = requests.get(token_endpoint, parameters)
+            
+            flash(f"Token response: {token_response.text}", category='warning')
+            
+        
+        return render_template("login.html", referrer=request.referrer, code=code)
         
     if request.method == 'POST':
         
