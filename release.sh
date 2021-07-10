9a7338535cbe11a2c2a595e9bb6fb35efbb80091
ogoro <mi.ogoro@gmail.com>
2021-07-10 14:45:00 +0000

    V.0.28

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo  'Clearing release-folder'
+echo
+rm -r ~/environment/release-folder/*
+
+echo  'Copying app to release-folder'
+echo
+cp -r ~/environment/m1/app/* ~/environment/release-folder/
+
+ENV_PATH=~/environment/m1/app/core-service/core/.env
+echo  'ENV file path:' $ENV_PATH
+echo
+
+VERSION=$(cat $ENV_PATH | grep -o -P '(?<=VERSION = ).*')
+echo  'Release VERSION:' $VERSION
+echo
+
+cd ~/environment/release-folder/
+
+git add .
+git commit -m "M1 Core $VERSION"
+git push --set-upstream origin main
