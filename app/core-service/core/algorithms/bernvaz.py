04841862a8c85e3f74060f9ab1fd6d43d67ebc4b
ogoro <mi.ogoro@gmail.com>
2021-07-13 09:12:05 +0000

    qiskit - add bernvaz to flask

--- app/core-service/core/algorithms/bernvaz.py
+++ app/core-service/core/algorithms/bernvaz.py
@@ -1,3 +1,56 @@
-def bernvaz(secret):
+from qiskit import *
 
-    return {'Secret:': secret}
+# from matplotlib import pyplot as plt
+# from qiskit.tools.visualization import plot_histogram
+
+def bernvaz(secret):
+    
+# def bernvaz(*args, **kwargs):
+    
+#     print('Args:', args)
+#     print('Kwargs:', kwargs)
+
+    HIDDEN_DIGITS = str(secret)
+    
+    print("Circuit:")
+    
+    hidden_len = len(HIDDEN_DIGITS)
+    
+    input_qubit_indices = range(hidden_len)
+    measure_bits_indices = input_qubit_indices
+    
+    output_qubit_index = hidden_len
+    
+    all_qubit_indices = range(hidden_len + 1)
+    
+    
+    circuit = QuantumCircuit(hidden_len + 1, hidden_len)
+    
+    circuit.x(output_qubit_index)
+    circuit.h(all_qubit_indices)
+    
+    circuit.barrier()
+    
+    for input_qubit_index, digit in enumerate(reversed(HIDDEN_DIGITS)):
+        if digit == '1':
+            circuit.cx(input_qubit_index, output_qubit_index)
+            
+    circuit.barrier()
+            
+    circuit.h(input_qubit_indices)
+    
+    circuit.measure(input_qubit_indices, measure_bits_indices)
+    
+    # circuit.draw(output='mpl')
+    # circuit.draw()
+    
+    print(circuit)
+    
+    simulator = Aer.get_backend('qasm_simulator')
+    result = execute(circuit, backend=simulator, shots=1).result()
+    
+    counts = result.get_counts()
+    
+    print("Possible hidden digits:", counts)
+    
+    return {'Counts:': counts}
