68d138c68aa118e55be80fb04a5b4c29bf56e7bf
ogoro <mi.ogoro@gmail.com>
2021-07-19 16:00:49 +0000

    thread - add multiple threads

--- app/core-service/core/thread.py
+++ app/core-service/core/thread.py
@@ -12,14 +12,8 @@ def thread_function(name):
     logging.info("Thread %s: finishing", name)
     
 
-if __name__ == "__main__":
+def launch_one_daemon_thread():
     
-    format = "%(asctime)s: %(message)s"
-    
-    logging.basicConfig(format=format, 
-                        level=logging.INFO,
-                        datefmt="%H:%M:%S")
-
     logging.info("Main    : before creating thread")
     
     x = threading.Thread(target=thread_function, args=(1,), daemon=True)
@@ -30,6 +24,45 @@ if __name__ == "__main__":
     
     logging.info("Main    : wait for the thread to finish")
     
-    # x.join()
+    x.join()
     
     logging.info("Main    : all done")
+    
+    
+
+def launch_multiple_threads():
+
+    threads = list()
+    
+    for index in range(3):
+        
+        logging.info("Main    : create and start thread %d.", index)
+        
+        x = threading.Thread(target=thread_function, args=(index,))
+        
+        threads.append(x)
+        
+        x.start()
+
+    for index, thread in enumerate(threads):
+        
+        logging.info("Main    : before joining thread %d.", index)
+        
+        thread.join()
+        
+        logging.info("Main    : thread %d done", index)
+
+    
+
+if __name__ == "__main__":
+    
+    format = "%(asctime)s: %(message)s"
+    
+    logging.basicConfig(format=format, 
+                        level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    launch_multiple_threads()
+    
+    
+    
