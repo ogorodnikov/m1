c6b2ef93bc1300b661b2839798ae34deb0ee817f
ogoro <mi.ogoro@gmail.com>
2021-07-23 22:41:57 +0000

    grover - add qiskit oracle

--- app/core-service/core/algorithms/grover.py
+++ app/core-service/core/algorithms/grover.py
@@ -3,80 +3,96 @@ from qiskit.providers.ibmq import least_busy
 from qiskit.tools.monitor import backend_overview, job_monitor
 
 
-# from qiskit_textbook.problems import grover_problem_oracle
+from qiskit_textbook.problems import grover_problem_oracle
 
-
+import math
+import numpy as np
 
 def grover(run_values):
     
-    qubit_count = 4
-    patterns = (2, 1), #(0, 2)
-    
-    circuit = QuantumCircuit(qubit_count)
-    
-    for qubit in range(qubit_count):
-        circuit.h(qubit)
-    
-    for pattern in patterns:
-        circuit.cz(*pattern)
+    qubit_count = 3
+    solutions = (0, 1), #(2, 3)
     
     elements_count = 2 ** qubit_count
-    patterns_count = len(patterns)
+    solutions_count = len(solutions)
     
-    repetitions =  (elements_count / patterns_count) ** 0.5 * 3.14 / 4
+    repetitions =  (elements_count / solutions_count) ** 0.5 * 3.14 / 4
     repetitions_count = int(repetitions)
     
+    iterations_count = math.floor(np.pi * np.sqrt(2 ** qubit_count / solutions_count) / 4)
+    
     print(f'GROVER elements_count: {elements_count}')
+    print(f'GROVER solutions_count: {solutions_count}')
     print(f'GROVER repetitions: {repetitions}')
     print(f'GROVER repetitions_count: {repetitions_count}')
+    print(f'GROVER iterations_count: {iterations_count}')
     
-    for i in range(repetitions_count):
+    oracle = grover_problem_oracle(qubit_count, variant=10, print_solutions=True)
     
-        circuit.barrier()
-        
-        for qubit in range(qubit_count):
-            circuit.h(qubit)
-    
-        for qubit in range(qubit_count):
-            circuit.x(qubit)
-            
-        for qubit in range(qubit_count-1):
-            circuit.i(qubit)
-    
-        circuit.h(qubit_count-1)
-        circuit.mct(list(range(qubit_count-1)), qubit_count-1)
-        circuit.h(qubit_count-1)
-        
-        for qubit in range(qubit_count-1):
-            circuit.i(qubit)
-    
-        for qubit in range(qubit_count):
-            circuit.x(qubit)
-    
-        for qubit in range(qubit_count):
-            circuit.h(qubit)
-        
+    # print(oracle.decompose())
+    # print(oracle.decompose().decompose())
+    # print(oracle.decompose().decompose().decompose())
 
-    circuit.measure_all()
+
+    for repetitions_count in range(1, 4):
+        
+        print(f'GROVER repetitions_count: {repetitions_count}')
+        
+        circuit = QuantumCircuit(qubit_count)
+        
+        circuit.append(oracle, [0, 1, 2])
     
-    print(circuit)
+        # for qubit in range(qubit_count):
+        #     circuit.h(qubit)
+        
+        # for pattern in patterns:
+        #     circuit.cz(*pattern)
+    
+        for i in range(repetitions_count):
+        
+            circuit.barrier()
+            
+            for qubit in range(qubit_count):
+                circuit.h(qubit)
+        
+            for qubit in range(qubit_count):
+                circuit.x(qubit)
+                
+            for qubit in range(qubit_count-1):
+                circuit.i(qubit)
+        
+            circuit.h(qubit_count-1)
+            circuit.mct(list(range(qubit_count-1)), qubit_count-1)
+            circuit.h(qubit_count-1)
+            
+            for qubit in range(qubit_count-1):
+                circuit.i(qubit)
+        
+            for qubit in range(qubit_count):
+                circuit.x(qubit)
+        
+            for qubit in range(qubit_count):
+                circuit.h(qubit)
+            
+    
+        circuit.measure_all()
+        
+        print(circuit.draw(output='text', fold=200))
     
     
-    # run
-    
-    backend = Aer.get_backend('qasm_simulator')
-    
-    job = execute(circuit, backend=backend, shots=1000)
-    
-    result = job.result()
-    
-    counts = result.get_counts()
-    
-    
-    # oracle = grover_problem_oracle(8, variant=10, print_solutions=True)
+        # run
+        
+        backend = Aer.get_backend('qasm_simulator')
+        
+        job = execute(circuit, backend=backend, shots=1000)
+        
+        result = job.result()
+        
+        counts = result.get_counts()
+        
+        [print(f'{state}: {count}') for state, count in sorted(counts.items())]
 
     return counts
     
-result = grover('11')
+result = grover('bonya')
 
-[print(f'{state}: {count}') for state, count in sorted(result.items())]
