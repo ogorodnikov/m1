efeb5d6b491d6c544bb1e45ba0da54c4d6a47641
ogoro <mi.ogoro@gmail.com>
2021-08-17 17:05:39 +0000

    simon - complete simon circuit

--- app/core-service/core/algorithms/simon.py
+++ app/core-service/core/algorithms/simon.py
@@ -4,39 +4,44 @@ from qiskit import QuantumCircuit
 def build_simon_oracle(period, masquerade=True):
     
     input_qubits_count = len(period)
-    
     output_qubits_count = input_qubits_count
     qubits_count = input_qubits_count + output_qubits_count
     
     input_qubits = range(input_qubits_count)
+    output_qubits = range(output_qubits_count)
     qubits = range(qubits_count)
     
     oracle = QuantumCircuit(qubits_count)
     oracle.name = 'Simon Oracle'
     
     
+    for input_qubit, output_qubit in zip(input_qubits, output_qubits):
+        
+        oracle.cx(input_qubit, output_qubit)
+        
     
-#     for state, secret_digit in truth_table.items():
+    is_one_to_one_mapping = '1' in period
+    
+    if is_one_to_one_mapping:
         
-#         if secret_digit == '0':
-#             continue
+        return oracle
         
-#         for state_digit_index, state_digit in enumerate(state):
-#             if state_digit == '0':
-#                 oracle.x(state_digit_index)
-                
-#         oracle.mct(list(input_qubits), output_qubit)
+    
+    first_period_one_bit = period.find('1')
+    
+    for output_qubit, digit in enumerate(period, input_qubits_count):
         
-#         for state_digit_index, state_digit in enumerate(state):
-#             if state_digit == '0':
-#                 oracle.x(state_digit_index)    
+        if digit == '1':
+            
+            oracle.cx(first_period_one_bit, output_qubit)
+        
+    return oracle
+    
+    if masquerade:
+        
+        oracle.barrier()
+    
 
-#         oracle.barrier()
-        
-    
-#     return oracle
-    
-    
 def simon(run_values, task_log):
         
     input_period = run_values.get('period')
@@ -55,50 +60,39 @@ def simon(run_values, task_log):
     measure_bits = range(measure_bits_count)
 
 
-#     simon_oracle = build_simon_oracle(period, task_log)
+    simon_oracle = build_simon_oracle(period)
     
 
-#     circuit = QuantumCircuit(all_qubits_count, measure_bits_count)
+    circuit = QuantumCircuit(qubits_count, measure_bits_count)
     
-#     for input_qubit in input_qubits:    
-#         circuit.h(input_qubit)
+    for input_qubit in input_qubits:    
+        circuit.h(input_qubit)
         
-#     circuit.x(output_qubit)
-#     circuit.h(output_qubit)
-    
-
-#     circuit.append(truth_table_oracle, all_qubits)
-    
+    circuit.append(simon_oracle, all_qubits)
         
-#     for input_qubit in input_qubits:    
-#         circuit.h(input_qubit)
+    for input_qubit in input_qubits:    
+        circuit.h(input_qubit)
         
 #     circuit.barrier()
-        
 
-#     qubits_measurement_list = list(reversed(input_qubits))
+    qubits_measurement_list = list(reversed(input_qubits))
     
-#     circuit.measure(qubits_measurement_list, measure_bits)
+    circuit.measure(qubits_measurement_list, measure_bits)
     
     
-#     task_log(f'DJ input_secret: {input_secret}')
-#     task_log(f'DJ full_secret: {full_secret}')
-#     task_log(f'DJ secret: {secret}')
-#     task_log(f'DJ secret_bit_len: {secret_bit_len}')
-#     task_log(f'DJ secret_len: {secret_len}')
+    task_log(f'SIMON input_period: {input_period}')
+    task_log(f'SIMON period: {period}')
+
+    task_log(f'SIMON input_qubits_count: {input_qubits_count}')
+    task_log(f'SIMON all_qubits_count: {all_qubits_count}')
+    task_log(f'SIMON qubits_measurement_list: {qubits_measurement_list}')
     
-#     task_log(f'DJ input_qubits_count: {input_qubits_count}')
-#     task_log(f'DJ all_qubits_count: {all_qubits_count}')
-#     task_log(f'DJ output_qubit: {output_qubit}')
-#     task_log(f'DJ qubits_measurement_list: {qubits_measurement_list}')
+    task_log(f'SIMON simon_oracle: {simon_oracle}')
+    task_log(f'SIMON circuit: \n{circuit}')
     
-#     task_log(f'DJ truth_table: {truth_table}')
-#     task_log(f'DJ truth_table_oracle: \n{truth_table_oracle}')
-#     task_log(f'DJ circuit: \n{circuit}')
-    
-#     task_log(f'DJ if majority of counts is all 0 - secret is constant')
-#     task_log(f'DJ if majority of counts is any other state - secret is balanced')
-#     task_log(f'DJ if counts are distributed via multiple states - probably secret is unbalanced')
+#     task_log(f'SIMON if majority of counts is all 0 - secret is constant')
+#     task_log(f'SIMON if majority of counts is any other state - secret is balanced')
+#     task_log(f'SIMON if counts are distributed via multiple states - probably secret is unbalanced')
     
 
-#     return circuit
+    return circuit
