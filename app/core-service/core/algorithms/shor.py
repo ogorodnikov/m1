100aa94aef27aa0e9dcd733687fa5ebd52011064
ogoro <mi.ogoro@gmail.com>
2021-09-10 14:18:28 +0000

    shor - add classical period finding

--- app/core-service/core/algorithms/shor.py
+++ app/core-service/core/algorithms/shor.py
@@ -26,22 +26,37 @@ def shor(run_values, task_log):
     number_input = run_values.get('number')
     number = int(number_input)
     
+    period = None
+    coprime = None
+    
     random_pick = randint(0, number)
     
     factor = gcd(number, random_pick)
     
     if factor != 1:
         
-        factors = factor, number // factor
-    
+        factor_p = factor
+
     else:
         
         coprime = random_pick
         
         period = get_period_classical(number, coprime)
         
+        if period % 2:
+            
+            ValueError("Period is odd")
+            
+        factor_p = gcd(random_pick ** (period // 2) + 1, number)
+        
+    
+    factor_q = number // factor_p
     
     
     task_log(f'SHOR number: {number}')
+    task_log(f'SHOR random_pick: {random_pick}')
     task_log(f'SHOR coprime: {coprime}')
     task_log(f'SHOR period: {period}')
+    
+    task_log(f'SHOR factor_p: {factor_p}')
+    task_log(f'SHOR factor_q: {factor_q}')
