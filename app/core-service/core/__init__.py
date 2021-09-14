d066c2ca265aa8e396bedf95b578dea979cbe7cc
ogoro <mi.ogoro@gmail.com>
2021-09-14 09:48:01 +0000

    gunicorn - add config module

--- app/core-service/core/__init__.py
+++ app/core-service/core/__init__.py
@@ -24,10 +24,10 @@ from core import engine
 
 runner = engine.Runner()
 
-from core import telegram
+# from core import telegram
 
-bot = telegram.Bot()
-bot.start()
+# bot = telegram.Bot()
+# bot.start()
 
 config.clear_figures_folder(app)
 
@@ -36,7 +36,7 @@ from core import routes
     
 
 
-from gunicorn.app.base import BaseApplication
+from gunicorn.app.base import BaseApplication, Application
 from gunicorn.workers.sync import SyncWorker
 
 
@@ -46,7 +46,8 @@ class CustomWorker(SyncWorker):
         
         app.logger.info(f'INIT handle_quit {self, sig, frame}')
         
-        # self.app.application.stop(sig)
+        self.app.application.stop(sig)
+        
         super().handle_quit(sig, frame)
 
     def run(self):
@@ -54,13 +55,11 @@ class CustomWorker(SyncWorker):
         app.logger.info(f'INIT run {self}')
         
         # self.app.application.start()
+        
         super().run()
 
 
-print(CustomWorker)
-
-
-class GunicornApp(BaseApplication):
+class GunicornApp(Application):
 
     def __init__(self, app, options=None):
         self.options = options or {}
@@ -71,33 +70,16 @@ class GunicornApp(BaseApplication):
         for key, value in self.options.items():
             self.cfg.set(key.lower(), value)
 
-        # self.cfg.set('worker_class', 'core.CustomWorker')
+        self.cfg.set('worker_class', 'core.CustomWorker')
 
     def load(self):
         return self.application
 
 
-def when_ready(server):
-    app.logger.info(f'INIT Server is ready {server}')
-    
-def on_reload(server):
-    app.logger.info(f'INIT Server reloading {server}')
-
-    
-gunicorn_options = {'bind': '0.0.0.0:8080',
-                    'access_log_format': "%(h)s | %(r)s %(s)s",
-                    'accesslog': '-',
-                    'workers': 2,
-                    'reload': 'true',
-                    'when_ready': when_ready,
-                    'on_reload': on_reload,
-                    # 'access_log_format': "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs",
-                    # 'threads': 1,
-                    # 'daemon': 'true',
-                   }
-
-gunicorn_app = GunicornApp(app, gunicorn_options)
 
 
-        
-    
+gunicorn_app = GunicornApp(app)
+
+gunicorn_app.load_config_from_module_name_or_filename('python:core.configuration.gunicorn')
+
+
