7bd00be901794ba24ebb47f55430a771548d29b1
ogoro <mi.ogoro@gmail.com>
2021-11-06 02:10:01 +0000

    test_main - mock initializers

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,8 +4,15 @@ import logging
 from _pytest.monkeypatch import MonkeyPatch
 
 from core.main import Main
+
+from core.app import FlaskApp
+from core.config import Config
+from core.routes import Routes
+from core.dynamo import Dynamo
 from core.runner import Runner
+from core.cognito import Cognito
 from core.telegram import Bot
+from core.facebook import Facebook
 
 
 ###   Logging   ###
@@ -42,15 +49,34 @@ def monkeypatch_module(request):
     
 
 @pytest.fixture(autouse=True, scope="module")
-def mocks(monkeypatch_module):
+def mocks(monkeypatch_module, stub):
     
-    def stub(*args, **kwargs):
-        pass
+    def test(*args, **kwargs):
+        raise UserWarning
+    
+    
+    class MockFlaskApp:
         
+        class MockConfig:
+            
+            def from_object(config):
+                pass
+        
+        config = MockConfig
+    
+    
+    from flask import Config as C
+    from flask import Flask
+    
     monkeypatch_module.setattr(Bot, "start", stub)
     monkeypatch_module.setattr(Runner, "start", stub)
-
-
-
-
-
+    monkeypatch_module.setattr(C, "from_object", stub)
+    
+    monkeypatch_module.setattr(FlaskApp, "__init__", Flask.__init__)
+    monkeypatch_module.setattr(Config, "__init__", stub) 
+    monkeypatch_module.setattr(Routes, "__init__", stub) 
+    monkeypatch_module.setattr(Dynamo, "__init__", stub) 
+    monkeypatch_module.setattr(Runner, "__init__", stub) 
+    monkeypatch_module.setattr(Cognito, "__init__", stub) 
+    monkeypatch_module.setattr(Bot, "__init__", stub) 
+    monkeypatch_module.setattr(Facebook, "__init__", stub)
