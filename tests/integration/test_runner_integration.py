f1b929159a577011c6dbec3ce7a3c7c7b49c420d
ogoro <mi.ogoro@gmail.com>
2021-11-08 18:33:36 +0000

    test_runner_integration - add start stop and plot statevector tests

--- tests/integration/test_runner_integration.py
+++ tests/integration/test_runner_integration.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from qiskit import QuantumCircuit
@@ -11,6 +12,15 @@ from test_runner import test_task
 from test_runner import get_test_task
 
 
+def test_start_stop(runner):
+    
+    runner.start()
+    assert os.environ.get('RUNNER_STATE') == 'Running'
+    
+    runner.stop()
+    assert os.environ.get('RUNNER_STATE') == 'Stopped'
+
+
 def test_execute_task(runner, monkeypatch, stub):
     
     monkeypatch.setattr(Runner, "monitor_job", stub)
@@ -19,8 +29,16 @@ def test_execute_task(runner, monkeypatch, stub):
     test_backend = runner.simulator_backend
     
     runner.execute_task(task_id=None, circuit=test_circuit, backend=test_backend)
+
+
+@pytest.mark.filterwarnings("ignore::PendingDeprecationWarning")
+@pytest.mark.filterwarnings("ignore::::69")
+def test_plot_statevector_figure(runner, test_run_result):
+    test_statevector = test_run_result.get_statevector(decimals=0)
     
-    
+    runner.plot_statevector_figure(statevector=test_statevector, task_id=None)
+        
+
 ###   Fixtures   ###
 
 @pytest.fixture(scope="module", autouse=True)
@@ -38,3 +56,16 @@ def set_mocks(mock, get_test_task, stub):
 @pytest.fixture
 def runner():
     return Runner(db=Dynamo())
+    
+    
+@pytest.fixture
+def test_run_result():
+    
+    class TestRunResult:
+        def get_counts(self):
+            return {'test_state': 0}
+            
+        def get_statevector(self, decimals):
+            return [0, 0, 1, 0]
+                
+    return TestRunResult()
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -48,11 +48,11 @@ def run_tests_with_coverage():
     # pytest.main([test_path + '/test_facebook.py', '-v', '-x', '--durations=0'])
     # pytest.main([test_path + '/test_main.py', '-v', '-x', '--durations=0'])
     # pytest.main([test_path + '/test_routes.py', '-v', '-x', '--durations=0'])
-    # pytest.main([test_path + '/test_runner.py', '-v', '-x', '-m not slow', '--durations=0'])
+    pytest.main([test_path + '/test_runner.py', '-v', '-x', '-m not slow', '--durations=0'])
     # pytest.main([test_path + '/test_telegram.py', '-v', '-x', '--durations=0'])
     # pytest.main([test_path + '/integration/test_telegram_integration.py', '-v', '-x', '--durations=0'])
     # pytest.main([test_path + '/integration/test_routes_integration.py', '-v', '-x', '--durations=0'])
-    pytest.main([test_path + '/integration/test_runner_integration.py', '-v', '-x', '--durations=0'])
+    # pytest.main([test_path + '/integration/test_runner_integration.py', '-v', '-x', '--durations=0'])
 
 
 
