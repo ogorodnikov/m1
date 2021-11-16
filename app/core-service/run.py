5cf86d58221e867077a84f5a4ca038167a4fb0e1
ogoro <mi.ogoro@gmail.com>
2021-11-16 10:14:44 +0000

    algorithm - add bervaz diagram

--- app/core-service/run.py
+++ app/core-service/run.py
@@ -1,15 +1,7 @@
-# from core.app import create_app
-
-
-# app = create_app()
-
-# app.run_with_gunicorn()
-
-# app.run_with_development_server()
-
-
 from core.main import Main
 
 main = Main()
 
-main.app.run_with_gunicorn()
+# main.app.run_with_gunicorn()
+
+main.app.run_with_development_server()
