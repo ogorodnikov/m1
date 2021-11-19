907f484436e965a9c23b3fbad081bdd570a39db3
ogoro <mi.ogoro@gmail.com>
2021-11-19 15:39:44 +0000

    shor - update classical factorization

--- app/core-service/core/algorithms/shor.py
+++ app/core-service/core/algorithms/shor.py
@@ -6,60 +6,9 @@ from qiskit import QuantumCircuit, Aer
 from core.algorithms.qft import build_qft_circuit
 
 
-def get_period_classical(modulo, x):
-    
-    period = 1
-    modular_exponent = x
-    
-    while modular_exponent != 1:
-        
-        modular_exponent = (modular_exponent * x) % modulo
-        period += 1
-        
-    return period
+MAX_EXPONENTIAL_BASE_PICKS = 10
 
 
-def shor_classical(run_values, task_log):
-    
-    number_input = run_values.get('number')
-    number = int(number_input)
-    
-    period = None
-    coprime = None
-    
-    random_pick = randint(0, number)
-    
-    factor = gcd(number, random_pick)
-    
-    if factor != 1:
-        
-        factor_p = factor
-
-    else:
-        
-        coprime = random_pick
-        
-        period = get_period_classical(number, coprime)
-        
-        if period % 2: ValueError("Period is odd")
-            
-        factor_p = gcd(random_pick ** (period // 2) + 1, number)
-        
-    
-    factor_q = number // factor_p
-    
-    
-    task_log(f'SHOR number: {number}')
-    task_log(f'SHOR random_pick: {random_pick}')
-    task_log(f'SHOR coprime: {coprime}')
-    task_log(f'SHOR period: {period}')
-    
-    task_log(f'SHOR factor_p: {factor_p}')
-    task_log(f'SHOR factor_q: {factor_q}')
-    
-    return factor_p, factor_q
-    
-    
 def shor(run_values, task_log):
     
     shor_classical(run_values, task_log)
@@ -68,3 +17,81 @@ def shor(run_values, task_log):
     circuit.name = 'Shor Circuit'
     
     return circuit
+    
+    
+def shor_classical(run_values, task_log):
+    
+    number_input = run_values.get('number')
+    number = int(number_input)
+    
+    task_log(f'SHOR number: {number}')
+    
+    factor = None
+    
+    retry_index = 0
+    
+    while not factor or retry_index > MAX_EXPONENTIAL_BASE_PICKS:
+        
+        factor = get_factor_classical(number, task_log)
+        
+        retry_index += 1
+        
+    if not factor:
+        
+        task_log(f'SHOR factor not found during {MAX_EXPONENTIAL_BASE_PICKS} tries')         
+        
+        return
+        
+    
+    factor_p, factor_q = factor, number // factor
+    
+    task_log(f'SHOR factor_p: {factor_p}')   
+    task_log(f'SHOR factor_q: {factor_q}')
+    
+    return factor_p, factor_q
+    
+    
+        
+        
+def get_factor_classical(number, task_log):
+    
+    exponentiation_base = randint(0, number)
+    
+    task_log(f'SHOR exponentiation_base: {exponentiation_base}')
+    
+    factor = gcd(number, exponentiation_base)
+    
+    task_log(f'SHOR factor: {factor}')    
+    
+    if factor == 1:
+        
+        period = get_period_classical(number, exponentiation_base)
+
+        task_log(f'SHOR period: {period}')
+        
+        if period % 2:
+            
+            task_log(f'SHOR period is odd')
+            
+            return
+            
+        factor = gcd(exponentiation_base ** (period // 2) + 1, number)
+        
+    return factor
+
+
+def get_period_classical(number, exponentiation_base):
+    
+    period = 1
+    
+    modulo = number
+    modular_exponent = exponentiation_base
+    
+    while modular_exponent != 1:
+        
+        modular_exponent = (modular_exponent * exponentiation_base) % modulo
+        period += 1
+        
+    return period
+    
+    
