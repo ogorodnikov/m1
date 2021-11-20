a6804d280138820c0363eb058ffdd6d9fca1573d
ogoro <mi.ogoro@gmail.com>
2021-11-20 23:51:37 +0000

    shor - add qubit initialization

--- app/core-service/core/algorithms/shor.py
+++ app/core-service/core/algorithms/shor.py
@@ -1,6 +1,8 @@
 # from math import gcd
 # from random import randint
 
+from math import log
+
 from qiskit import Aer
 from qiskit import QuantumCircuit
 
@@ -23,73 +25,59 @@ class Shor:
         print(f'SHOR number: {number}')
         
         exponentiation_base = 7
+        precision = 8
         
-        print(f'SHOR exponentiation_base: {exponentiation_base}')
-        
-        # self.exponent_by_squaring(2, 1000000)
-        
-        print(2**1000000)
-        
-        # phase = self.estimate_phase(a)
+        phase = self.estimate_phase(exponentiation_base, precision)
     
         # circuit = QuantumCircuit(1, 1)
         # circuit.name = 'Shor Circuit'
         
         # return circuit
         
-    def exponent_by_squaring(self, exponentiation_base, power):
-        
-        power_binary = list(map(int, bin(power)[2:]))
 
-        print(f'exponentiation_base: {exponentiation_base}')
-        print(f'power: {power}')
-        print(f'power_binary: {power_binary}')
-        
-        result = 1
-        
-        for power_bit in power_binary:
-            
-            result **= 2
-            result *= exponentiation_base ** power_bit
-            
-            print(f'SHOR power_bit: {power_bit}')
-            print(f'SHOR result: {result}')
-        
-        return result
-        
-
-    def estimate_phase(self, exponentiation_base):
+    def estimate_phase(self, exponentiation_base, precision):
         
         """ https://qiskit.org/textbook/ch-algorithms/shor.html """
         
-        counting_qubits_count = 8
+        print(f'SHOR exponentiation_base: {exponentiation_base}')
+        print(f'SHOR precision: {precision}')
+        
+        counting_qubits_count = precision
         
         counting_qubits = range(counting_qubits_count)
         
-        ancilla_qubits_count = 4
+        print(f'SHOR counting_qubits: {counting_qubits}')
+        
+        ancilla_qubits_count = int(log(precision, 2)) + 1
+        
+        print(f'SHOR ancilla_qubits_count: {ancilla_qubits_count}')
         
         ancilla_qubits = range(counting_qubits_count, 
                                counting_qubits_count + ancilla_qubits_count)
-                             
-        register_qubit = range(counting_qubits_count + ancilla_qubits_count,
-                               counting_qubits_count + ancilla_qubits_count + 1)
-                               
+
+        print(f'SHOR ancilla_qubits: {ancilla_qubits}')
+        
+        register_qubit = counting_qubits_count + ancilla_qubits_count - 1
+
+        print(f'SHOR register_qubit: {register_qubit}')
+        
+                                       
         circuit = QuantumCircuit(counting_qubits_count + ancilla_qubits_count, 
                                  counting_qubits_count)
-        
+                                 
         for counting_qubit in counting_qubits:
             circuit.h(counting_qubit)
             
-        circuti.name = ''
+        circuit.name = ''
         
         circuit.x(register_qubit)
         
-        for ancilla_index in ancilla_qubits:
+        # for ancilla_index in ancilla_qubits:
             
-            circuit.append(self.controlled_amod15(exponentiation_base, 2**ancilla_index),
-                           [ancilla_index] + [counting_qubits])
+        #     circuit.append(self.controlled_amod15(exponentiation_base, 2**ancilla_index),
+        #                   [ancilla_index] + [counting_qubits])
                            
-        print(f'SHOR circuit: {circuit}')
+        print(f'SHOR circuit: \n{circuit}')
         
 
     def controlled_amod15(self, exponentiation_base, power):
