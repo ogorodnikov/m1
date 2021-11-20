d90f0ef064172db9d8bbf7402caca50d5096dccf
ogoro <mi.ogoro@gmail.com>
2021-11-20 20:53:00 +0000

    shor - add camod circuit

--- app/core-service/core/algorithms/shor.py
+++ app/core-service/core/algorithms/shor.py
@@ -4,34 +4,54 @@ from random import randint
 from qiskit import Aer
 from qiskit import QuantumCircuit
 
-from core.algorithms.qft import build_qft_circuit
+# from core.algorithms.qft import build_qft_circuit
+from qft import build_qft_circuit
 
 
 MAX_EXPONENTIAL_BASE_PICKS = 10
 
 
 
-def shor(run_values, task_log):
+def controlled_amod15(a, power):
     
-    number_input = run_values.get('number')
-    number = int(number_input)
+    # Controlled multipliaction by a mod 15 
+    # for a in 2, 7, 8, 11, 13
     
-    task_log(f'SHOR number: {number}')
+    camod_circuit = QuantumCircuit(4)        
     
-    a = 7
+    for iteration in range(power):
+        
+        if a in [2,13]:
+            camod_circuit.swap(0,1)
+            camod_circuit.swap(1,2)
+            camod_circuit.swap(2,3)
+            
+        elif a in [7,8]:
+            camod_circuit.swap(2,3)
+            camod_circuit.swap(1,2)
+            camod_circuit.swap(0,1)
+            
+        elif a == 11:
+            camod_circuit.swap(1,3)
+            camod_circuit.swap(0,2)
+            
+        if a in [7,11,13]:
+            for q in range(4):
+                circuit.x(q)
+                
+    camod_gate = camod_circuit.to_gate()
     
-    task_log(f'SHOR a: {a}')
+    camod_gate.name = f"{a} ** {power} mod 15"
     
-    phase = qpe_a_mod_15(a)
+    controlled_camod_gate = camod_gate.control()
     
+    task_log(f'SHOR camod_circuit: {camod_circuit}')  
+    task_log(f'SHOR camod_gate: {camod_gate}')  
+    task_log(f'SHOR controlled_camod_gate: {controlled_camod_gate}')  
+    
+    return controlled_camod_gate
     
 
-    circuit = QuantumCircuit(1, 1)
-    circuit.name = 'Shor Circuit'
-    
-    return circuit
-    
-    
 def qpe_a_mod15(a):
     
     """ https://qiskit.org/textbook/ch-algorithms/shor.html """
@@ -55,6 +75,46 @@ def qpe_a_mod15(a):
         circuit.h(counting_qubit)
     
     circuit.x(register_qubit)
+    
+    for ancilla_index in ancilla_qubits:
+        
+        circuit.append(controlled_amod15(a, 2**ancilla_index),
+                       [ancilla_index] + [counting_qubits])
+                       
+    task_log(f'SHOR circuit: {circuit}')
+
+
+def shor(run_values, task_log):
+    
+    task_log(f'>>> SHOR')    
+    
+    number_input = run_values.get('number')
+    number = int(number_input)
+    
+    task_log(f'SHOR number: {number}')
+    
+    a = 7
+    
+    task_log(f'SHOR a: {a}')
+    
+    
+    task_log(f'>>> SHOR phase')
+    
+    phase = qpe_a_mod15(a)
+    
+    task_log(f'>>> SHOR after phase')        
+
+    circuit = QuantumCircuit(1, 1)
+    circuit.name = 'Shor Circuit'
+    
+    return circuit
+
+
+run_values = {'number': '330023'}
+    
+shor(run_values, print)
+        
+        
 
      
      
