69b3a01ef6ce108bd0911d228a29ba1126b63cb7
ogoro <mi.ogoro@gmail.com>
2021-11-22 22:15:20 +0000

    teleport - add basic gates

--- app/core-service/core/algorithms/teleport.py
+++ app/core-service/core/algorithms/teleport.py
@@ -1,9 +1,25 @@
 from qiskit import QuantumCircuit
 
 
+def add_bell_pair(circuit, qubit_a, qubit_b):
+    
+    circuit.h(qubit_a)
+    circuit.cx(qubit_a, qubit_b)
+    
+
+def add_alice_gates(circuit, qubit_a, qubit_b):
+    
+    circuit.cx(qubit_a, qubit_b)
+    circuit.h(qubit_a)
+    
+    
 def teleport(run_values, task_log):
     
     state = 0 + 1j
+    
+    alice_source_qubit = 0
+    alice_entangled_qubit = 1
+    bob_entangled_qubit = 2
     
     quantum_register = QuantumRegister(3, name="q")
     
@@ -14,7 +30,20 @@ def teleport(run_values, task_log):
                              classical_register_z,
                              classical_register_x)
     
-    circuit.initialize(state, quantum_register[0])
+    circuit.initialize(state, alice_source_qubit)
+    
+    circuit.barrier()
+    
+    add_bell_pair(circuit, alice_entangled_qubit, bob_entangled_qubit)
+    
+    circuit.barrier()    
+
+    add_alice_gates(circuit, alice_source_qubit, alice_entangled_qubit) 
+    
+    circuit.barrier()
+    
+    
+    
     
     task_log(f'TELEPORT circuit: \n{circuit}')
     
