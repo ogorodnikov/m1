5b658b6b77229698ad1a9f87ef889c53ea6137ed
ogoro <mi.ogoro@gmail.com>
2021-12-01 18:30:38 +0000

    home - add github link

--- app/core-service/run.py
+++ app/core-service/run.py
@@ -2,6 +2,6 @@ from core.main import Main
 
 main = Main()
 
-main.app.run_with_gunicorn()
+# main.app.run_with_gunicorn()
 
-# main.app.run_with_development_server()
+main.app.run_with_development_server()
