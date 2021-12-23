004a3af6fd9ed3f4e344f118ff468a519672788d
ogoro <mi.ogoro@gmail.com>
2021-12-23 00:59:01 +0000

    counting - complete grover iteration circuit

--- app/core-service/core/algorithms/counting.py
+++ app/core-service/core/algorithms/counting.py
@@ -13,46 +13,52 @@ except ModuleNotFoundError:
     from core.algorithms.qft import create_qft_circuit
     
     
-def build_unitary_phase_oracle(secrets, elements_count):
+def build_unitary_phase_oracle(secrets, qubits_count):
     
-    # phase_oracle_circuit = QuantumCircuit(qubits_count)
+    phase_oracle_circuit = QuantumCircuit(qubits_count + 1)
+    
+    ancilla_qubit = qubits_count
+    
+    phase_oracle_circuit.x(ancilla_qubit)
+    phase_oracle_circuit.h(ancilla_qubit)
     
     for secret in secrets:
         
-        # diffuser_circuit.mct(list(range(1, qubits_count)), 0)
+        control_qubits = [qubit_index for qubit_index, secret_digit in enumerate(secret) 
+                          if secret_digit == '1']
         
+        phase_oracle_circuit.mct(control_qubits, ancilla_qubit)
         
-        secret_string = ''.join('1' if letter == '1' else '0' for letter in secret)
-        secret_index = int(secret_string, 2)
-        diagonal_elements[secret_index] = -1
-
-    phase_oracle = Diagonal(diagonal_elements)
-    phase_oracle.name = 'Phase Oracle'
+    phase_oracle_circuit.h(ancilla_qubit)
+    phase_oracle_circuit.x(ancilla_qubit)
+        
+    phase_oracle_circuit.name = 'Unitary Phase Oracle'
     
-    return phase_oracle
+    print(f'COUNT secrets: {secrets}')
+    print(f'COUNT phase_oracle_circuit:\n{phase_oracle_circuit}')
+    
+    return phase_oracle_circuit
     
 
 def build_grover_iteration(qubits_count, secrets):
     
     qubits = range(qubits_count)
+    ancilla_qubit = qubits_count
     
-    elements_count = 2 ** qubits_count
+    grover_iteration_circuit = QuantumCircuit(qubits_count + 1)
     
-    grover_iteration_circuit = QuantumCircuit(qubits_count)
-    
-    phase_oracle = build_phase_oracle(secrets=secrets, 
-                                      elements_count=elements_count)
-                                      
+    phase_oracle = build_unitary_phase_oracle(secrets=secrets, qubits_count=qubits_count)
     diffuser = build_diffuser(qubits_count=qubits_count)
     
-    grover_iteration_circuit.append(phase_oracle, qubits)
-    grover_iteration_circuit.append(diffuser, qubits)
-
-    print(f'COUNT grover_iteration_circuit:\n{grover_iteration_circuit}')
-    print(f'COUNT phase_oracle:\n{phase_oracle.decompose()}')
-    print(f'COUNT phase_oracle:\n{phase_oracle.decompose().decompose().decompose()}')
+    phase_oracle_gate = phase_oracle.to_gate()
+    diffuser_gate = diffuser.to_gate()
     
-    quit()
+    grover_iteration_circuit.append(phase_oracle_gate, [*qubits, ancilla_qubit])
+    grover_iteration_circuit.append(diffuser_gate, qubits)
+
+    print(f'COUNT [*qubits, ancilla_qubit]: {[*qubits, ancilla_qubit]}')
+    print(f'COUNT grover_iteration_circuit:\n{grover_iteration_circuit}')
+    print(f'COUNT phase_oracle:\n{phase_oracle}')
     
     return grover_iteration_circuit
     
@@ -65,10 +71,12 @@ def quantum_counting(run_values, task_log):
     qubits_count = len(max(secrets, key=len))
     
     grover_iteration_circuit = build_grover_iteration(qubits_count, secrets)
-    # grover_iteration_gate = grover_iteration_circuit.to_gate()
-    controlled_grover_iteration = grover_iteration_circuit.control()
+    
+    grover_iteration_gate = grover_iteration_circuit.to_gate()
+    # controlled_grover_iteration = grover_iteration_circuit.control()
     
     task_log(f'COUNT run_values: {run_values}')
-    task_log(f'COUNT grover_iteration_circuit: {grover_iteration_circuit}')
-    # task_log(f'COUNT grover_iteration_gate: {grover_iteration_gate}')
-    task_log(f'COUNT controlled_grover_iteration: {controlled_grover_iteration}')
+    task_log(f'COUNT grover_iteration_circuit:\n{grover_iteration_circuit}')
+    
+    task_log(f'COUNT grover_iteration_gate: {grover_iteration_gate}')
+    # task_log(f'COUNT controlled_grover_iteration: {controlled_grover_iteration}')
