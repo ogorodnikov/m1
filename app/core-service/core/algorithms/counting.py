7b3b016cbdc0980b4a36a6e6ea9f790cae1cbeee
ogoro <mi.ogoro@gmail.com>
2021-12-29 13:46:12 +0000

    counting - add example grover iteration

--- app/core-service/core/algorithms/counting.py
+++ app/core-service/core/algorithms/counting.py
@@ -13,58 +13,36 @@ except ModuleNotFoundError:
     from core.algorithms.qft import create_qft_circuit
     
     
-def build_unitary_phase_oracle(secrets, qubits_count):
-    
-    phase_oracle_circuit = QuantumCircuit(qubits_count + 1)
-    
-    ancilla_qubit = qubits_count
-    
-    phase_oracle_circuit.x(ancilla_qubit)
-    phase_oracle_circuit.h(ancilla_qubit)
-    
-    for secret in secrets:
-        
-        control_qubits = [qubit_index for qubit_index, secret_digit in enumerate(secret) 
-                          if secret_digit == '1']
-        
-        phase_oracle_circuit.mct(control_qubits, ancilla_qubit)
-        
-    phase_oracle_circuit.h(ancilla_qubit)
-    phase_oracle_circuit.x(ancilla_qubit)
-        
-    phase_oracle_circuit.name = 'Unitary Phase Oracle'
-    
-    print(f'COUNT secrets: {secrets}')
-    print(f'COUNT phase_oracle_circuit:\n{phase_oracle_circuit}')
-    
-    return phase_oracle_circuit
-    
+def example_grover_iteration(qubits_count, secrets):
 
-def build_grover_iteration(qubits_count, secrets):
+    circuit = QuantumCircuit(4)
     
-    qubits = range(qubits_count)
-    ancilla_qubit = qubits_count
+    # Oracle
+    circuit.h([2,3])
+    circuit.ccx(0,1,2)
+    circuit.h(2)
+    circuit.x(2)
+    circuit.ccx(0,2,3)
+    circuit.x(2)
+    circuit.h(3)
+    circuit.x([1,3])
+    circuit.h(2)
+    circuit.mct([0,1,3],2)
+    circuit.x([1,3])
+    circuit.h(2)
     
-    grover_iteration_circuit = QuantumCircuit(qubits_count + 1)
+    circuit.barrier()
     
-    phase_oracle = build_unitary_phase_oracle(secrets=secrets, qubits_count=qubits_count)
-    diffuser = build_diffuser(qubits_count=qubits_count)
+    # Diffuser
+    circuit.h(range(3))
+    circuit.x(range(3))
+    circuit.z(3)
+    circuit.mct([0,1,2],3)
+    circuit.x(range(3))
+    circuit.h(range(3))
+    circuit.z(3)
     
-    grover_iteration_circuit.append(phase_oracle, [*qubits, ancilla_qubit])
-    grover_iteration_circuit.append(diffuser, qubits)
-    
-    print(f'COUNT phase_oracle:\n{phase_oracle}')
-    print(f'COUNT [*qubits, ancilla_qubit]: {[*qubits, ancilla_qubit]}')
-    print(f'COUNT grover_iteration_circuit:\n{grover_iteration_circuit}')
-    
-    
-    
-    controlled_phase_oracle = phase_oracle.control()
-    diffuser_gate = diffuser.to_gate()
-    # controlled_diffuser_gate = diffuser_gate.control()
-
-    
-    return grover_iteration_circuit
+    return circuit
     
 
 def quantum_counting(run_values, task_log):
@@ -74,7 +52,7 @@ def quantum_counting(run_values, task_log):
     
     qubits_count = len(max(secrets, key=len))
     
-    grover_iteration_circuit = build_grover_iteration(qubits_count, secrets)
+    grover_iteration_circuit = example_grover_iteration(qubits_count, secrets)
     
     task_log(f'COUNT run_values: {run_values}')
     task_log(f'COUNT grover_iteration_circuit:\n{grover_iteration_circuit}')
