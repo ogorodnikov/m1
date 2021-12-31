b62f1006aa78743ad2ae339ef090da0b65bd501b
ogoro <mi.ogoro@gmail.com>
2021-12-31 19:02:29 +0000

    bb84 - add minimal modules

---
+++ app/core-service/core/algorithms/bb84.py
@@ -0,0 +1,10 @@
+from qiskit import QuantumCircuit
+
+
+def bb84(run_values, task_log):
+    
+    
+    
+    # Logs
+    
+    task_log(f'BB84 run_values: {run_values}')
--- app/core-service/core/algorithms/runners/run_bb84.py
+++ app/core-service/core/algorithms/runners/run_bb84.py
@@ -2,4 +2,13 @@ import sys
 
 sys.path.append('/home/ec2-user/environment/m1/app/core-service/core/algorithms')
 
-from qft import *
+
+from qiskit import Aer
+from qiskit import execute
+
+from bb84 import bb84
+
+
+run_values = {'secret': '1010'}
+
+circuit = bb84(run_values=run_values, task_log=print)
