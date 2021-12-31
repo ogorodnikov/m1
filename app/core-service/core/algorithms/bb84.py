143f6d0deea493a22f6b111dbcab675abcab9e17
ogoro <mi.ogoro@gmail.com>
2021-12-31 19:28:07 +0000

    bb84 - add alice side circuit

--- app/core-service/core/algorithms/bb84.py
+++ app/core-service/core/algorithms/bb84.py
@@ -4,6 +4,30 @@ from qiskit import QuantumCircuit
 def bb84(run_values, task_log):
     
     
+    # Alice side
+    
+    alice_bits = '1010'
+    
+    alice_bases = 'XZXX'
+    
+    qubits = []
+    
+    for bit, base in zip(alice_bits, alice_bases):
+        
+        qubit = QuantumCircuit(1, 1)
+        
+        if bit == '1':
+            qubit.x(0)
+            
+        if base == 'X':
+            qubit.h(0)
+            
+        qubit.barrier()
+        
+        qubits.append(qubit)
+        
+    
+        
     
     # Logs
     
