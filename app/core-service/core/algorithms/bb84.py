7e6b8504908d93b769d25d7504fa61d0b3d8e5f0
ogoro <mi.ogoro@gmail.com>
2022-01-02 21:56:00 +0000

    bb84 - complete post processing

--- app/core-service/core/algorithms/bb84.py
+++ app/core-service/core/algorithms/bb84.py
@@ -10,15 +10,12 @@ def bb84(run_values, task_log):
     alice_bits = run_values['alice_bits']
     
     alice_bases = run_values['alice_bases']
+    
     eve_bases = run_values['eve_bases']
     bob_bases = run_values['bob_bases']
-    
     bob_bases = run_values['bob_bases']
 
-    sample_indices_input = run_values['sample_indices'].split(',')
-    sample_indices = list(map(int, map(str.strip, sample_indices_input)))
-    
-    sample_len = len(sample_indices)
+    sample_indices_input = run_values['sample_indices']
     
     message_len = len(alice_bits)
     
@@ -38,7 +35,7 @@ def bb84(run_values, task_log):
     circuit.barrier()
     
         
-    # Eve's side
+    # Eve side
     
     for qubit_index, eve_base in enumerate(eve_bases):
         
@@ -63,186 +60,87 @@ def bb84(run_values, task_log):
         circuit.measure(qubit_index, message_len + qubit_index)
             
         circuit.barrier()
-            
-    # simulator = Aer.get_backend('aer_simulator')
-    
-    # qobj = assemble(circuit, shots=1, memory=True)
-    
-    # result = simulator.run(qobj).result()
-    
-    # all_bits = result.get_memory()[0]
-    
-    all_bits = '010011010011'
-    
-    bob_bits = list(reversed(all_bits[:message_len]))
-
-    eve_bits = list(reversed(all_bits[-message_len:]))
-
-    # print(result.get_memory())
-    # print(result.get_counts())
-    # print(eve_bits)
-    
-    # quit()
-    
-
-    # Filter bits
-    
-    alice_key = []
-    
-    for alice_base, bob_base, alice_bit in zip(alice_bases, bob_bases, alice_bits):
         
-        if alice_base == bob_base:
             
-            alice_key.append(alice_bit)
-            
-    
-    bob_key = []
-    
-    for alice_base, bob_base, bob_bit in zip(alice_bases, bob_bases, bob_bits):
-        
-        if alice_base == bob_base:
-            
-            bob_key.append(bob_bit)
-            
-    
-    # Sample comparison
-    
-    alice_sample = []
-    bob_sample = []
-    
-    for sample_index in sample_indices:
-        
-        alice_sample.append(alice_key[sample_index])
-        bob_sample.append(bob_key[sample_index])
-    
-    samples_fit = alice_sample == bob_sample
-    
-    key_length = len(alice_key)
-    
-    
-    # Reliability calculation
-    
-    bases_count = max(map(len, map(set, (alice_bases, eve_bases, bob_bases))))
-    
-    basis_match_probability = 1 / bases_count
-    
-    states = [0, 1]
-    
-    states_count = len(set(states))
-    
-    state_match_probability = 1 / states_count
-    
-    single_qubit_evesdropping_detected_probability = basis_match_probability * state_match_probability
-    single_qubit_evesdropping_undetected_probability = 1 - single_qubit_evesdropping_detected_probability
-    
-    total_evesdropping_undetected_probability = single_qubit_evesdropping_undetected_probability ** sample_len
-    total_evesdropping_detected_probability = 1 - total_evesdropping_undetected_probability
-
-    
     # Logs
     
     task_log(f'BB84 run_values: {run_values}')
     
     task_log(f'BB84 alice_bits: {alice_bits}')
+    
     task_log(f'BB84 alice_bases: {alice_bases}')
-    
     task_log(f'BB84 eve_bases: {eve_bases}')
-    task_log(f'BB84 eve_bits: {eve_bits}')
-    
     task_log(f'BB84 bob_bases: {bob_bases}')
-    task_log(f'BB84 bob_bits: {bob_bits}')
     
-    task_log(f'BB84 alice_key: {alice_key}')
-    task_log(f'BB84 bob_key: {bob_key}')
-    
-    task_log(f'BB84 alice_sample: {alice_sample}')
-    task_log(f'BB84 bob_sample: {bob_sample}')
-    
-    task_log(f'BB84 samples_fit: {samples_fit}')
-    task_log(f'BB84 key_length: {key_length}')
+    task_log(f'BB84 sample_indices_input: {sample_indices_input}')
+    task_log(f'BB84 message_len: {message_len}')
     
     task_log(f'BB84 circuit:\n{circuit}')
-    
-    task_log(f'BB84 all_bits: {all_bits}')
- 
-    task_log(f'BB84 single_qubit_evesdropping_undetected_probability: {single_qubit_evesdropping_undetected_probability}')  
-    task_log(f'BB84 single_qubit_evesdropping_detected_probability: {single_qubit_evesdropping_detected_probability}')  
-    
-    task_log(f'BB84 total_evesdropping_undetected_probability: {total_evesdropping_undetected_probability}')  
-    task_log(f'BB84 total_evesdropping_detected_probability: {total_evesdropping_detected_probability}')
     
     return circuit
 
 
 def bb84_post_processing(run_data, task_log):
     
+    # Inputs
+    
     run_values = run_data['Run Values']
     counts = run_data['Result']['Counts']
-    
-    max_state = max(counts, key=counts.get)
-    
-    # Inputs
     
     alice_bits = run_values['alice_bits']
     
     alice_bases = run_values['alice_bases']
     eve_bases = run_values['eve_bases']
     bob_bases = run_values['bob_bases']
-    
-    bob_bases = run_values['bob_bases']
 
-    sample_indices_input = run_values['sample_indices'].split(',')
-    sample_indices = list(map(int, map(str.strip, sample_indices_input)))
+    sample_indices_input = run_values['sample_indices']
+    
+    
+    # Parameters
+    
+    sample_indices_split = sample_indices_input.split(',')
+    sample_indices = list(map(int, map(str.strip, sample_indices_split)))
     
     sample_len = len(sample_indices)
-    
     message_len = len(alice_bits)
     
+    max_state = max(counts, key=counts.get)
     
-    all_bits = max_state
-    
-    bob_bits = list(reversed(all_bits[:message_len]))
+    bob_bits = ''.join(reversed(max_state[:message_len]))
+    eve_bits = ''.join(reversed(max_state[-message_len:]))
 
-    eve_bits = list(reversed(all_bits[-message_len:]))
 
-    # print(result.get_memory())
-    # print(result.get_counts())
-    # print(eve_bits)
+    # Keys
     
-    # quit()
-    
-
-    # Filter bits
-    
-    alice_key = []
+    alice_key = ''
     
     for alice_base, bob_base, alice_bit in zip(alice_bases, bob_bases, alice_bits):
         
         if alice_base == bob_base:
             
-            alice_key.append(alice_bit)
+            alice_key += alice_bit
             
     
-    bob_key = []
+    bob_key = ''
     
     for alice_base, bob_base, bob_bit in zip(alice_bases, bob_bases, bob_bits):
         
         if alice_base == bob_base:
             
-            bob_key.append(bob_bit)
+            bob_key += bob_bit
             
     
     # Sample comparison
     
-    alice_sample = []
-    bob_sample = []
+    alice_sample = ''
+    bob_sample = ''
     
     for sample_index in sample_indices:
         
-        alice_sample.append(alice_key[sample_index])
-        bob_sample.append(bob_key[sample_index])
-    
-    samples_fit = alice_sample == bob_sample
+        alice_sample += alice_key[sample_index]
+        bob_sample += bob_key[sample_index]
+
+    samples_match = alice_sample == bob_sample
     
     key_length = len(alice_key)
     
@@ -268,11 +166,8 @@ def bb84_post_processing(run_data, task_log):
     
     # Logs
     
-    
     task_log(f'BB84 counts: {counts}')
     task_log(f'BB84 max_state: {max_state}')
-    
-    
     
     task_log(f'BB84 run_values: {run_values}')
     
@@ -291,12 +186,9 @@ def bb84_post_processing(run_data, task_log):
     task_log(f'BB84 alice_sample: {alice_sample}')
     task_log(f'BB84 bob_sample: {bob_sample}')
     
-    task_log(f'BB84 samples_fit: {samples_fit}')
+    task_log(f'BB84 samples_match: {samples_match}')
     task_log(f'BB84 key_length: {key_length}')
     
-    
-    task_log(f'BB84 all_bits: {all_bits}')
- 
     task_log(f'BB84 single_qubit_evesdropping_undetected_probability: {single_qubit_evesdropping_undetected_probability}')  
     task_log(f'BB84 single_qubit_evesdropping_detected_probability: {single_qubit_evesdropping_detected_probability}')  
     
