19b8ffc989e0c9f82f9d678bb25cc4ee93625967
ogoro <mi.ogoro@gmail.com>
2022-01-02 21:43:39 +0000

    bb84 - add duplicating post processing

--- app/core-service/core/algorithms/runners/run_bb84.py
+++ app/core-service/core/algorithms/runners/run_bb84.py
@@ -6,7 +6,7 @@ sys.path.append('/home/ec2-user/environment/m1/app/core-service/core/algorithms'
 from qiskit import Aer
 from qiskit import execute
 
-from bb84 import bb84
+from bb84 import bb84, bb84_post_processing
 
 
 # run_values = {'alice_bits': '10101011',
@@ -23,3 +23,16 @@ run_values = {'alice_bits': '101010',
 
 
 circuit = bb84(run_values=run_values, task_log=print)
+
+
+backend = Aer.get_backend('aer_simulator')
+
+job = execute(circuit, backend, shots=1024)
+
+counts = job.result().get_counts()
+
+
+run_data = {'Result': {'Counts': counts}, 
+            'Run Values': run_values}
+
+bb84_post_processing(run_data=run_data, task_log=print)
