c9104b02fe58cac36defab9414c142ea32a66e3c
ogoro <mi.ogoro@gmail.com>
2022-01-03 17:55:18 +0000

    walk - add basic circuit

--- app/core-service/core/algorithms/walk.py
+++ app/core-service/core/algorithms/walk.py
@@ -5,12 +5,24 @@ from qiskit import ClassicalRegister
 
 def walk(run_values, task_log):
     
+    
+    # Registers
+    
     theta_register = QuantumRegister(4, 'theta')
     node_register = QuantumRegister(4, 'node')
     coin_register = QuantumRegister(4, 'coin')
     auxilary_register = QuantumRegister(4, 'auxilary')
     
     measure_register = ClassicalRegister(4, 'measure')
+    
+    circuit = QuantumCircuit(theta_register,
+                             node_register,
+                             coin_register,
+                             auxilary_register,
+                             measure_register)
+                             
+    circuit.h(node_register)
+    circuit.h(coin_register)
     
     # Logs
     
