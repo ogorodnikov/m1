afd1bded07a6013ee918af7684b5041b29de73db
ogoro <mi.ogoro@gmail.com>
2022-01-03 18:03:47 +0000

    walk - add iterations

--- app/core-service/core/algorithms/walk.py
+++ app/core-service/core/algorithms/walk.py
@@ -6,14 +6,21 @@ from qiskit import ClassicalRegister
 def walk(run_values, task_log):
     
     
+    # Inputs
+    
+    iterations_count = 2
+    
     # Registers
     
     theta_register = QuantumRegister(4, 'theta')
     node_register = QuantumRegister(4, 'node')
-    coin_register = QuantumRegister(4, 'coin')
-    auxilary_register = QuantumRegister(4, 'auxilary')
+    coin_register = QuantumRegister(2, 'coin')
+    auxilary_register = QuantumRegister(1, 'auxilary')
     
     measure_register = ClassicalRegister(4, 'measure')
+    
+    
+    # Circuit
     
     circuit = QuantumCircuit(theta_register,
                              node_register,
@@ -21,9 +28,27 @@ def walk(run_values, task_log):
                              auxilary_register,
                              measure_register)
                              
-    circuit.h(node_register)
-    circuit.h(coin_register)
+    phase_oracle_qubits = [*node_register, *coin_register]
+    
+    phase_estimation_qubits = [*theta_register, 
+                               *node_register,
+                               *coin_register,
+                               *auxilary_register]
+    
+    circuit.h(phase_oracle_qubits)
+    
+    phase_oracle_gate = QuantumCircuit(6)
+    phase_estimation_gate = QuantumCircuit(11)
+    
+    for iteration in range(iterations_count):
+        
+        circuit.append(phase_oracle_gate, phase_oracle_qubits)
+        circuit.append(phase_estimation_gate, phase_estimation_qubits)
+        
     
     # Logs
     
     task_log(f'WALK run_values: {run_values}')
+    
+    # task_log(f'WALK phase_oracle_qubits: {phase_oracle_qubits}')
+    # task_log(f'WALK phase_estimation_qubits: {phase_estimation_qubits}')
