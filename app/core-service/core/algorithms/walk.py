85fbf62dbf78129120b6af8cc8d0864933b60ceb
ogoro <mi.ogoro@gmail.com>
2022-01-03 17:52:45 +0000

    walk - add registers

--- app/core-service/core/algorithms/walk.py
+++ app/core-service/core/algorithms/walk.py
@@ -5,6 +5,13 @@ from qiskit import ClassicalRegister
 
 def walk(run_values, task_log):
     
+    theta_register = QuantumRegister(4, 'theta')
+    node_register = QuantumRegister(4, 'node')
+    coin_register = QuantumRegister(4, 'coin')
+    auxilary_register = QuantumRegister(4, 'auxilary')
+    
+    measure_register = ClassicalRegister(4, 'measure')
+    
     # Logs
     
     task_log(f'WALK run_values: {run_values}')
