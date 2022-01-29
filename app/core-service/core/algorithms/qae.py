901e8d00336e74c5baedf9080b077b5e442d3d18
ogoro <mi.ogoro@gmail.com>
2022-01-29 01:55:21 +0000

    qae - add reference estimation

--- app/core-service/core/algorithms/qae.py
+++ app/core-service/core/algorithms/qae.py
@@ -23,6 +23,36 @@ def qae(run_values, task_log):
     bernoulli_a.ry(theta_p, 0)
     bernoulli_q.ry(2 * theta_p, 0)
     
+    # Reference Estimation
+    
+    from qiskit.algorithms import EstimationProblem
+
+    problem = EstimationProblem(
+        state_preparation=bernoulli_a,
+        grover_operator=bernoulli_q,
+        objective_qubits=[0]
+    )
+    
+    
+    from qiskit import BasicAer
+    from qiskit.utils import QuantumInstance
+    
+    backend = BasicAer.get_backend("statevector_simulator")
+    quantum_instance = QuantumInstance(backend)
+    
+    
+    from qiskit.algorithms import AmplitudeEstimation
+
+    ae = AmplitudeEstimation(
+        num_eval_qubits=3,
+        quantum_instance=quantum_instance
+    )
+    
+    ae_result = ae.estimate(problem)
+    
+    result = ae_result.estimation
+    samples = ae_result.samples
+    
     # Logs
     
     task_log(f'QAE run_values: {run_values}')
@@ -33,3 +63,6 @@ def qae(run_values, task_log):
     
     task_log(f'QAE bernoulli_a:\n{bernoulli_a}')
     task_log(f'QAE bernoulli_q:\n{bernoulli_q}')
+    
+    task_log(f'QAE result: {result}')
+    task_log(f'QAE samples: {samples}')
