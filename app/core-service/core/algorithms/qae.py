3c5f43ecd24ea05658c0c55708abdd9001f7efa3
ogoro <mi.ogoro@gmail.com>
2022-01-29 01:39:52 +0000

    qae - add probability input

--- app/core-service/core/algorithms/qae.py
+++ app/core-service/core/algorithms/qae.py
@@ -2,6 +2,12 @@ def qae(run_values, task_log):
     
     ''' https://qiskit.org/documentation/finance/tutorials/00_amplitude_estimation.html '''
     
+    # Input
+    
+    input_probability = run_values.get('bernoulli_probability')
+    probability = float(input_probability)
+    
     # Logs
     
     task_log(f'QAE run_values: {run_values}')
+    task_log(f'QAE probability: {probability}')
