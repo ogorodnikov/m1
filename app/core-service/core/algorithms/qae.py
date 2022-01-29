8874e5733c25fc422f737f2a811d7b939ecc7178
ogoro <mi.ogoro@gmail.com>
2022-01-29 01:33:08 +0000

    qae - add basic module

---
+++ app/core-service/core/algorithms/qae.py
@@ -0,0 +1,7 @@
+def walk(run_values, task_log):
+    
+    ''' https://qiskit.org/documentation/finance/tutorials/00_amplitude_estimation.html '''
+    
+    # Logs
+    
+    task_log(f'QAE run_values: {run_values}')
