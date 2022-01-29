4173af7f4c7170452e2e7edb11397d371834559a
ogoro <mi.ogoro@gmail.com>
2022-01-29 01:47:42 +0000

    qae - add theta p

--- app/core-service/core/algorithms/qae.py
+++ app/core-service/core/algorithms/qae.py
@@ -1,3 +1,8 @@
+from math import asin
+
+from qiskit import QuantumCircuit
+
+
 def qae(run_values, task_log):
     
     ''' https://qiskit.org/documentation/finance/tutorials/00_amplitude_estimation.html '''
@@ -7,7 +12,19 @@ def qae(run_values, task_log):
     input_probability = run_values.get('bernoulli_probability')
     probability = float(input_probability)
     
+    
+    # Circuits
+    
+    bernoulli_a = QuantumCircuit(1)
+    
+    theta_p = 2 * asin(probability ** 0.5)
+    
+    bernoulli_a.ry(theta_p, 0)
+    
     # Logs
     
     task_log(f'QAE run_values: {run_values}')
+    
     task_log(f'QAE probability: {probability}')
+    
+    task_log(f'QAE theta_p: {theta_p}')
