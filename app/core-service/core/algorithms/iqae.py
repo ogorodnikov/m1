5f54ad1d63ffb33a5f22e26bc6d28695a9ffe048
ogoro <mi.ogoro@gmail.com>
2022-01-31 23:42:34 +0000

    iqae - add basic module

---
+++ app/core-service/core/algorithms/iqae.py
@@ -0,0 +1,111 @@
+from math import pi, sin, asin
+
+from qiskit import QuantumCircuit
+from qiskit import QuantumRegister
+from qiskit import ClassicalRegister
+
+try:
+    from qft import create_qft_circuit
+except ModuleNotFoundError:
+    from core.algorithms.qft import create_qft_circuit
+
+AMPLITUDE_DIGITS_COUNT = 7
+
+
+def iqae(run_values, task_log):
+    
+    """
+    Create Canonical Quantum Amplitude Estimation (QAE) circuit
+    
+    https://qiskit.org/documentation/finance/tutorials/00_amplitude_estimation.html
+    
+    """
+    
+    # Input
+    
+    input_bernoulli_probability = run_values.get('bernoulli_probability')
+    input_precision = run_values.get('precision')
+    
+    bernoulli_probability = float(input_bernoulli_probability)
+    precision = int(input_precision)
+    
+    
+    # Bernoulli Circuits
+    
+    theta_p = 2 * asin(bernoulli_probability ** 0.5)
+    
+    bernoulli_operator = QuantumCircuit(1)
+    bernoulli_operator.ry(theta_p, 0)
+    
+    bernoulli_diffuser = QuantumCircuit(1)
+    bernoulli_diffuser.ry(2 * theta_p, 0)
+    
+    controlled_bernoulli_diffuser = bernoulli_diffuser.control()
+    controlled_bernoulli_diffuser.name = 'Controlled Bernoulli Diffuser'
+    
+    bernoulli_operator.name = 'Bernoulli Operator'
+    bernoulli_diffuser.name = 'Bernoulli Diffuser'
+    controlled_bernoulli_diffuser.name = 'CBD'
+    
+
+    # QPE Circuit
+    
+    estimation_register = QuantumRegister(precision, 'estimation')
+    eigenstate_register = QuantumRegister(1, 'eigenstate')
+    
+    qpe_circuit = QuantumCircuit(estimation_register, eigenstate_register)
+    qpe_circuit.name = 'QPE'
+    
+    qpe_circuit.h(estimation_register)
+    
+    # QPE Iterations
+    
+    for estimation_qubit_index, estimation_qubit in enumerate(estimation_register):
+    
+        iterations_count = 2 ** estimation_qubit_index
+        
+        for iteration in range(iterations_count):
+        
+            iteration_qubits = [estimation_qubit, *eigenstate_register]
+            
+            qpe_circuit.append(controlled_bernoulli_diffuser, iteration_qubits)
+    
+    # IQFT
+    
+    iqft_circuit = create_qft_circuit(precision, inverted=True)
+    
+    qpe_circuit.append(iqft_circuit, estimation_register)
+    
+    
+    # QAE Circuit
+
+    measure_register = ClassicalRegister(precision, 'measurement')
+    
+    qae_circuit = QuantumCircuit(estimation_register, 
+                                 eigenstate_register, 
+                                 measure_register)
+    
+    qae_circuit.append(bernoulli_operator, eigenstate_register)
+    qae_circuit.append(qpe_circuit, [*estimation_register,
+                                     *eigenstate_register])
+
+    qae_circuit.measure(estimation_register, measure_register)
+    
+
+    # Logs
+    
+    task_log(f'QAE run_values: {run_values}\n')
+    
+    task_log(f'QAE bernoulli_probability: {bernoulli_probability}')
+    task_log(f'QAE precision: {precision}\n')
+    
+    task_log(f'QAE theta_p: {theta_p}\n')
+    
+    task_log(f'QAE bernoulli_operator:\n{bernoulli_operator}\n')
+    task_log(f'QAE bernoulli_diffuser:\n{bernoulli_diffuser}\n')
+    
+    task_log(f'QAE iqft_circuit: \n{iqft_circuit}\n')
+    task_log(f'QAE qpe_circuit: \n{qpe_circuit}\n')
+    task_log(f'QAE qae_circuit:\n{qae_circuit}\n')
+    
+    return qae_circuit
--- app/core-service/core/algorithms/runners/run_iqae.py
+++ app/core-service/core/algorithms/runners/run_iqae.py
@@ -5,7 +5,7 @@ sys.path.append('/home/ec2-user/environment/m1/app/core-service/core/algorithms'
 from qiskit import Aer
 from qiskit import execute
 
-from qae import qae, qae_post_processing
+from iqae import iqae
 
 
 RUN_VALUES = {'bernoulli_probability': '0.2', 
@@ -14,21 +14,21 @@ RUN_VALUES = {'bernoulli_probability': '0.2',
 
 # Circuit
 
-circuit = qae(run_values=RUN_VALUES, task_log=print)
+circuit = iqae(run_values=RUN_VALUES, task_log=print)
 
 
-# Run
+# # Run
 
-backend = Aer.get_backend('aer_simulator')
+# backend = Aer.get_backend('aer_simulator')
 
-job = execute(circuit, backend, shots=1024)
+# job = execute(circuit, backend, shots=1024)
 
-counts = job.result().get_counts()
+# counts = job.result().get_counts()
 
 
-# Post-processing
+# # Post-processing
 
-RUN_DATA = {'Result': {'Counts': counts}, 
-            'Run Values': RUN_VALUES}
+# RUN_DATA = {'Result': {'Counts': counts}, 
+#             'Run Values': RUN_VALUES}
 
-qae_post_processing(run_data=RUN_DATA, task_log=print)
+# iqae_post_processing(run_data=RUN_DATA, task_log=print)
