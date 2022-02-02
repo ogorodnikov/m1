8db6101207637a7f89dfb5337ee3720f9a436591
ogoro <mi.ogoro@gmail.com>
2022-02-02 15:35:41 +0000

    iqae - add iteration counts printouts

--- app/core-service/core/algorithms/iqae.py
+++ app/core-service/core/algorithms/iqae.py
@@ -167,7 +167,7 @@ def iqae(run_values, task_log):
     confint_method = "clopper_pearson"
     min_ratio = 2
     
-    accuracy = 0.01
+    accuracy = 0.0001
     width_of_cofidence_interval = 0.05
 
     epsilon = epsilon_target = accuracy
@@ -247,16 +247,40 @@ def iqae(run_values, task_log):
         iteration_shots = shots
         iteration_one_counts = one_counts
         
-        if iteration_number > 1:
+        if iteration_number > 0:
             
-            while (
-                powers[iteration_number - same_k_count] == powers[iteration_number]
-                and iteration_number >= same_k_count + 1
-            ):
+            last_different_power_index = iteration_number - same_k_count
+            last_different_power = powers[last_different_power_index]
+            current_power = powers[iteration_number]
+            
+            task_log(f'QAE powers: {powers}')
+            task_log(f'QAE one_shots_counts: {one_shots_counts}')
+            task_log(f'QAE iteration_number: {iteration_number}')
+            task_log(f'QAE iteration_one_counts: {iteration_one_counts}')
+            
+            task_log(f'QAE same_k_count: {same_k_count}')
+            task_log(f'QAE last_different_power_index: {last_different_power_index}')
+            task_log(f'QAE last_different_power: {last_different_power}')
+            task_log(f'QAE current_power: {current_power}')
+            
+            while (last_different_power == current_power
+                   and iteration_number >= same_k_count + 1):
                 
                 same_k_count += 1
-                iteration_shots += shots
-                iteration_one_counts += one_shots_counts[-same_k_count]
+                
+                last_different_one_shot_counts = one_shots_counts[-same_k_count]
+                
+                iteration_shots += shots                
+                iteration_one_counts += last_different_one_shot_counts
+                
+                last_different_power_index = iteration_number - same_k_count
+                last_different_power = powers[last_different_power_index]
+                current_power = powers[iteration_number]
+                
+                task_log(f'QAE     same_k_count: {same_k_count}')
+                task_log(f'QAE     iteration_shots: {iteration_shots}')
+                task_log(f'QAE     last_different_one_shot_counts: {last_different_one_shot_counts}')
+                task_log(f'QAE     iteration_one_counts: {iteration_one_counts}')
 
         # Ai
 
