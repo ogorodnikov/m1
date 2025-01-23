37006795c035442ddea7b804ad4a84d0c353f1f9
ogoro <mi.ogoro@gmail.com>
2025-01-23 14:26:48 +0000

    test - add telegram test token

--- tests/test_telegram.py
+++ tests/test_telegram.py
@@ -126,7 +126,7 @@ def test_echo_handler(bot, message):
 @pytest.fixture(scope="module", autouse=True)
 def set_mocks(mock, mock_env, user, stub, test_algorithm):
     
-    mock_env('TELEGRAM_TOKEN', 'test_token')
+    mock_env('TELEGRAM_TOKEN', 'test_token:1234')
     
     mock("core.telegram.Bot.get_me", lambda self: user)
     
