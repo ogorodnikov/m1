1bb0d70e31ec238c4fd478489ba440637b86dcb7
Mykhailo Ohorodnikov <mohor@softserveinc.com>
2025-01-27 18:30:59 +0200

    lint - add config linting

--- app/core-service/core/config.py
+++ app/core-service/core/config.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 
 class Config:
-    
+
     def __init__(self):
 
         load_dotenv()
@@ -18,16 +18,16 @@ class Config:
         self.VERSION = os.getenv('VERSION')
         self.SECRET_KEY = os.getenv('SECRET_KEY')
 
-        self.CORE_BUCKET = os.getenv('CORE_BUCKET')        
+        self.CORE_BUCKET = os.getenv('CORE_BUCKET')
         self.TASKS_TABLE_NAME = os.getenv('TASKS_TABLE_NAME')
         self.ALGORITHMS_TABLE_NAME = os.getenv('ALGORITHMS_TABLE_NAME')
-    
-        self.FACEBOOK_CLIENT_ID = os.getenv('FACEBOOK_CLIENT_ID')    
+
+        self.FACEBOOK_CLIENT_ID = os.getenv('FACEBOOK_CLIENT_ID')
         self.FACEBOOK_CLIENT_SECRET = os.getenv('FACEBOOK_CLIENT_SECRET')
 
         self.QISKIT_TOKEN = os.getenv('QISKIT_TOKEN')
         self.TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-        
+
         self.NLB_NAME = os.getenv('NLB_NAME')
         self.AWS_NLB = self.get_nlb_dns(self.NLB_NAME)
         os.environ['AWS_NLB'] = self.AWS_NLB
@@ -37,23 +37,22 @@ class Config:
 
         self.BACKEND_AVOID_STRING = 'ibmq_bogota'
         os.environ['BACKEND_AVOID_STRING'] = self.BACKEND_AVOID_STRING
-        
+
         self.JSONIFY_PRETTYPRINT_REGULAR = False
         self.PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
         self.CPU_COUNT = os.cpu_count()
-        
+
         # self.EXPLAIN_TEMPLATE_LOADING = True
-        
+
         # self.QISKIT_IBMQ_PROVIDER_LOG_LEVEL = 'DEBUG'
-        
 
     def get_nlb_dns(self, nlb_name):
-        
+
         load_balancers_response = self.elb_client.describe_load_balancers(Names=[nlb_name])
-        
+
         load_balancers = load_balancers_response['LoadBalancers']
-        
+
         nlb_dns = next(load_balancer['DNSName'] for load_balancer in load_balancers
                        if load_balancer['LoadBalancerName'] == nlb_name)
-                            
+
         return nlb_dns
